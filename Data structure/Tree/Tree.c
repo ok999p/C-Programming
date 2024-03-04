132ff5ce95a5731c95a42549c5b5dff795fb4965
ok999p <tachanon.srisook@gmail.com>
2024-03-05 01:17:24 +0700

    .

--- Data structure/Tree/Tree.c
+++ Data structure/Tree/Tree.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Tree
+struct tree
 {
-    struct Tree *left;
+    struct tree *left;
     int info;
-    struct Tree *right;
+    struct tree *right;
 };
+
+
+struct tree *treenode (int data) {
+    struct tree *node = (struct tree*)malloc(sizeof (struct tree));
+    if(node != NULL){
+    node ->info = data;
+    node ->left = NULL;
+    node ->right = NULL;
+    }
+}
+
+
+int main(){
+    struct tree *root = treenode(1);
+}
+
